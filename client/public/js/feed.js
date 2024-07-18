e64c71039ecdcedbf83c52e68dfbb983aef58b34
Roman D'Alfonso <nuccimoney@gmail.com>
2024-07-18 14:54:05 -0400

    delete functionality added

--- client/public/js/feed.js
+++ client/public/js/feed.js
@@ -8,28 +8,9 @@ for (let i = 0; i < tps.length; i++) {
     });
 }
 
+let delbut = document.getElementsByClassName("delete-button");
 
 
-
-
-/*
-function feedItem(title, body, linkUrl, imageUrl) {
-    this.Title = title;
-    this.Body = body;
-    this.linkUrl = linkUrl;
-    this.imageUrl = imageUrl;
-}
-
-
-// Create an array called currentStories
-let currentStories = [];
-
-// Create 3 instances of feedItem and add them to the array as elements
-currentStories.push(new feedItem("Marist Spring Concert!", "Click \"read more\" for more details about out lineup!", "https://www.youtube.com/watch?v=dQw4w9WgXcQ", "images/marist_spring_concert.png"));
-currentStories.push(new feedItem("Study in Dublin", "Study in the capital city of the Emerald Isle", "http://linkurl2.com", "images/dublin.jpg"));
-currentStories.push(new feedItem("Studiare in Firenze!", "Il programma nuovo di studio in Italia per gli studenti del primo anno.", "http://linkurl1.com", "images/firenze.jpg"));
-
-*/
 
 
 
@@ -37,7 +18,7 @@ currentStories.push(new feedItem("Studiare in Firenze!", "Il programma nuovo di 
 function displayItem() {
     // Get the newsfeed element
     let newsFeed = document.getElementById("newsFeed");
-
+    newsFeed.innerHTML = ``
     // Append the feed item element to the newsfeed
     for (let i = 0; i < currentStories.length; i++) {
 
@@ -48,6 +29,7 @@ function displayItem() {
             <h2 id='story_title'>${currentStories[i].title}</h2>
             <p id='story_body'>${currentStories[i].body}</p>
             <a id='read_more'href="${currentStories[i].linkUrl}">Read more</a>
+            <button class='delete-button'>Delete Item</button>
             </div>
             <a href="${currentStories[i].linkUrl}"><img id="photo" src="${currentStories[i].imageUrl}" alt="${currentStories[i].Title}"></a>
             <p></p>
@@ -66,11 +48,25 @@ window.addEventListener('load', () => {
             console.log(data)
             currentStories = data
             displayItem()
+            let delbut = document.getElementsByClassName('delete-button')
+            for (let i = 0; i < delbut.length; i++) {
+                delbut[i].addEventListener("click", () => {
+                    console.log('ah')
+                    fetch('/api/currentStories/' + i, {
+                        method: 'delete'
+                    }
+                    ).then(() => {
+                        console.log('ahhh')
+                        displayItem()
+                    });
+                })
+            };
         })
-        
+
         console.log("Page has loaded!");
     })
-    
+
 });
+
 
 
